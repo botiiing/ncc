63a8c538051ebd41a929f8aad20c28ba52b8b9d2
botiiing <sddsfv85df4@gmail.com>
2017-03-24 11:42:00 +0430

    Add files via upload

---
+++ bot/config.lua
@@ -0,0 +1,39 @@
+do local _ = {
+  enabled_plugins = {
+    "addrem",
+    "bc",
+    "supergroup",
+    "msg_checks",
+    "pin",
+    "owner",
+    "online",
+    "plugins",
+    "admin",
+    "id",
+    "del",
+    "clean",
+    "expiretime",
+    "filter",
+    "setlink",
+    "setrules",
+    "help",
+    "leave",
+    "lock_bots",
+    "me"
+  },
+  group = {
+    data = "bot/group.json"
+  },
+  robot = {
+    338105280,
+    0
+  },
+  sudo_users = {
+    170146015,
+    204507468,
+    196568905,
+    309970083
+  }
+}
+return _
+end
